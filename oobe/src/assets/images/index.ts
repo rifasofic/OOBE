5e26a21ec0d3cb886fa782dace6ae34985306b1e
Ismet Softic <ismet.softic@secomind.com>
2025-12-01 09:32:19 +0100

    Add image assets
    Add image assets for card icons
    
    Signed-off-by: Ismet Softic <ismet.softic@secomind.com>

--- oobe/src/assets/images/index.ts
+++ oobe/src/assets/images/index.ts
@@ -1,3 +1,33 @@
 import logo from "./seco-logo.png";
+import cardiology from "./cardiology.png";
+import factory from "./factory.png";
+import falling from "./falling.png";
+import interactiveSpace from "./interactive_space.png";
+import settingsEthernet from "./settings_ethernet.png";
+import vitalSigns from "./vital_signs.png";
+import warehouse from "./warehouse.png";
+import warning from "./warning.png";
+import corporateFare from "./corporate_fare.png";
+import gradient from "./gradient.png";
+import openJam from "./open_jam.png";
+import playCircle from "./play_circle.png";
+import touchLong from "./touch_long.png";
+import hd from "./hd.png";
 
-export { logo };
+export {
+  logo,
+  cardiology,
+  factory,
+  falling,
+  interactiveSpace,
+  settingsEthernet,
+  vitalSigns,
+  warehouse,
+  warning,
+  corporateFare,
+  openJam,
+  gradient,
+  hd,
+  playCircle,
+  touchLong,
+};
